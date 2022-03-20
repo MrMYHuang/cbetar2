6d320fe919ab7858c542dd84ba686068d954cf18
Meng-Yuan Huang <myh@live.com>
2022-03-20 20:28:25 +0800

    * Add files for F-Droid build.

---
+++ android/source/app/src/main/java/io/github/mrmyhuang/cbetar2/DelegationService.java
@@ -0,0 +1,18 @@
+package io.github.mrmyhuang.cbetar2;
+
+
+import com.google.androidbrowserhelper.locationdelegation.LocationDelegationExtraCommandHandler;
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+            registerExtraCommandHandler(new LocationDelegationExtraCommandHandler());
+        
+    }
+}
+
