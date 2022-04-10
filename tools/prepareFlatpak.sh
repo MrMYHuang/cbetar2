619ee8104a5161b0ff426d87ba158452beeac78f
Meng-Yuan Huang <myh@live.com>
2022-04-10 22:09:17 +0800

    * Fix flatpak script bugs.

--- tools/prepareFlatpak.sh
+++ tools/prepareFlatpak.sh
@@ -3,10 +3,13 @@ version=$(jq -r .version package.json)
 
 # Update flatpak source.
 file=${version}.tar.gz
+flatpakFile=flatpak/io.github.mrmyhuang.cbetar2.yml
+
 wget https://github.com/MrMYHuang/cbetar2/archive/${file}
 sha256=$(sha256sum ${file} | awk '{print $1}')
 rm ${file}
-sed "s/sha256: .*$/sha256: ${sha256}/" flatpak/io.github.mrmyhuang.cbetar2.yml
+sed "s/sha256: .*$/sha256: ${sha256}/" ${flatpakFile} -i
+sed "s/\(cbetar2\/archive\/\).*.tar.gz$/\1${file}/" ${flatpakFile} -i
 
 # Update packages.
 ./flatpak-node-generator.py --xdg-layout -o flatpak/generated-sources.json npm package-lock.json
