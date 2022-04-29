f298c18b4f5558fccd286faddb9c64cfbf53bc96
Meng-Yuan Huang <myh@live.com>
2022-04-29 09:36:27 +0800

    * Support Linux AppImage package.

--- srcElectron/Globals.ts
+++ srcElectron/Globals.ts
@@ -5,6 +5,7 @@ export const latestDownloadUrl = `${PackageInfos.repository}/releases/latest/dow
 export function backendAppPackageType() {
     if (fs.existsSync(`${process.resourcesPath}/IsWin.txt`)) return 'win';
     if (fs.existsSync(`${process.resourcesPath}/IsMac.txt`)) return 'mac';
+    if (fs.existsSync(`${process.resourcesPath}/IsAppImage.txt`)) return 'appImage';
     if (fs.existsSync(`${process.resourcesPath}/IsRpm.txt`)) return 'rpm';
     if (fs.existsSync(`${process.resourcesPath}/IsDeb.txt`)) return 'deb';
     if (fs.existsSync(`${process.resourcesPath}/IsSnap.txt`)) return 'snap';
