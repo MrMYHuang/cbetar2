f9a065cfe5c2df1e617b4ecac10d000fbf75c35b
Meng-Yuan Huang <myh@live.com>
2022-05-02 20:57:26 +0800

    * Update a script.

--- tools/prepareFlatpak.sh
+++ tools/prepareFlatpak.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
-# Update packages.
 
+# Update manifest.
+version=$(jq -r .version package.json)
+flatpakFile=flatpak/io.github.mrmyhuang.cbetar2.yml
+sed "s/tag: .*$/tag: ${version}/" ${flatpakFile} -i
+
+# Update offline packages.
 # From node folder of https://github.com/flatpak/flatpak-builder-tools.git
 ./flatpak-node-generator.py --electron-node-headers --xdg-layout -o flatpak/generated-sources.json npm package-lock.json
 
