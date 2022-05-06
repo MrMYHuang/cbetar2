f3075e06cff51f0a91e5fb6f65abe952c521869b
Meng-Yuan Huang <myh@live.com>
2022-05-06 09:36:38 +0800

    * Move file.

--- tools/prepareFlatpak.sh
+++ tools/prepareFlatpak.sh
@@ -7,7 +7,7 @@ sed "s/tag: .*$/tag: ${version}/" ${flatpakFile} -i
 
 # Update offline packages.
 # From node folder of https://github.com/flatpak/flatpak-builder-tools.git
-./flatpak-node-generator.py --electron-node-headers --xdg-layout -o flatpak/generated-sources.json npm package-lock.json
+./tools/flatpak-node-generator.py --electron-node-headers --xdg-layout -o flatpak/generated-sources.json npm package-lock.json
 
 # Verify metainfo.
 flatpak run org.freedesktop.appstream-glib validate ./buildElectron/io.github.mrmyhuang.cbetar2.metainfo.xml
